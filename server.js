f2071716cc81c69495c13a6cdfdf8094b6b275be
BuzRom <romanbuzukov@gmail.com>
2021-09-26 12:41:02 +0300

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults({ static: './build' });
-const port = process.env.PORT || 3001;
-
-// server.use('/', express.static(path.join(__dirname, 'build')))
+const middlewares = jsonServer.defaults({
+   static: './build'
+});
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT, () => {
+   console.log('Server is running');
+});
